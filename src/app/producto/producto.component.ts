50f77fb532f536fa2d27526b2fe4a2edf024a660
Juana Custodio <custodim.juana@gmail.com>
2020-12-23 20:30:25 -0500

    add producto

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -13,9 +13,12 @@ export class ProductoComponent implements OnInit {
   desktop: boolean;
   producto: Producto;
   id: string;
+  btnText: string;
 
   constructor(private activatedRoute: ActivatedRoute, private dataService: DataService, private firestore: AngularFirestore) {
+    this.desktop = true;
     this.id = '';
+    this.btnText = 'GUARDAR';
     this.producto = {
       id: '',
       nombre: '',
@@ -31,6 +34,10 @@ export class ProductoComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if (this.id === 'nuevo') {
+      this.btnText = 'AGREGAR';
+    }
+
     const productos = this.dataService.productos;
     productos.forEach((producto) => {
       if (producto.id === this.id) {
@@ -43,7 +50,13 @@ export class ProductoComponent implements OnInit {
 
   guardarProducto(): void {
     const {id, ...producto} = this.producto;
-    this.firestore.collection('Productos').doc(id).set(producto);
+
+    if (this.id === 'nuevo') {
+      this.firestore.collection('Productos').add(producto);
+    }
+    else {
+      this.firestore.collection('Productos').doc(id).set(producto);
+    }
   }
 
 }
