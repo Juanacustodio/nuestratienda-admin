782cfe5c41a34b94c366b677dbaef883be7303f3
Juana Custodio <custodim.juana@gmail.com>
2021-02-06 13:39:04 -0500

    logout

--- src/app/components/shared/menu-admin/menu-admin.component.ts
+++ src/app/components/shared/menu-admin/menu-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu-admin',
@@ -14,10 +16,14 @@ export class MenuAdminComponent implements OnInit {
   ];
 
 
-  constructor() { }
+  constructor(private cookies: CookieService, private router: Router) { }
 
   ngOnInit(): void {
   }
-  
+
+  logout() {
+    this.cookies.delete("token");
+    this.router.navigate(['/login']);
+  }
 
 }
