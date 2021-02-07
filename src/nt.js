2842cb672a352485d78426e4792659fd9234aa70
Juana Custodio <custodim.juana@gmail.com>
2021-02-06 22:26:01 -0500

    change nav

---
+++ src/nt.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+// MENU
+  function handleSmallScreens() {
+    document.querySelector('.navbar-toggler')
+      .addEventListener('click', () => {
+        let navbarMenu = document.querySelector('.navbar-menu')
+
+        if (navbarMenu.style.display === 'flex') {
+          navbarMenu.style.display = 'none'
+          return
+        }
+
+        navbarMenu.style.display = 'flex'
+      })
+  }
+
+  handleSmallScreens()
+});
