192cafab592eca552376caaf97d44f88a8cf1d1b
Juana Custodio <custodim.juana@gmail.com>
2021-02-12 09:08:05 -0500

    pedidos view

--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Pedido} from '../models/pedido';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-pedido',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  pedido: Pedido;
+  estados: any;
+  estadosColor: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private firestore: AngularFirestore) {
+    this.id = '';
+    this.pedido = {} as Pedido;
+    this.estados = {
+      1: 'Pendiente',
+      2: 'Finalizado',
+      3: 'Cancelado'
+    };
+    this.estadosColor = {
+      1: 'light',
+      2: 'success',
+      3: 'danger'
+    };
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params.id;
+    });
+  }
 
   ngOnInit(): void {
+    const pedido = this.firestore.collection('Pedidos').doc(this.id).valueChanges({idField: 'id'});
+    pedido.subscribe(params => {
+      const pedido = params as Pedido;
+      console.log(params.pedido);
+      this.pedido = {
+        id: pedido.id,
+        fecha: (new Date(params.fecha.toDate())).toLocaleString(),
+        estado: this.estados[pedido.estado],
+        estadoColor: this.estadosColor[pedido.estado],
+        total: pedido.total,
+        pedido: pedido.pedido,
+      };
+    });
   }
 
 }
