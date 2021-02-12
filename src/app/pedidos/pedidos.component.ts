752b284c36bca06b42cce8ce641df0626c757c96
Juana Custodio <custodim.juana@gmail.com>
2021-02-11 22:18:16 -0500

    finish pedidos view

--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AngularFirestore} from '@angular/fire/firestore';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-pedidos',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pedidos.component.scss']
 })
 export class PedidosComponent implements OnInit {
+  pedidos: Array<any> = [];
+  estados: any;
+  estadosColor: any;
 
-  constructor() { }
+  constructor(private router: Router, private firestore: AngularFirestore) {
+    this.estados = {
+      1: 'Pendiente',
+      2: 'Finalizado',
+      3: 'Cancelado'
+    };
+    this.estadosColor = {
+      1: 'light',
+      2: 'success',
+      3: 'danger'
+    };
+    firestore.collection('Pedidos').valueChanges({idField: 'id'})
+      .subscribe(pedidos => {
+        _.forEach(pedidos, (pedido) => {
+          const newPedido = {
+            id: pedido.id,
+            fecha: (new Date(pedido.fecha.seconds)).toLocaleString(),
+            estado: this.estados[pedido.estado],
+            estadoColor: this.estadosColor[pedido.estado],
+            total: pedido.total,
+            productos: pedido.pedido,
+          };
+          this.pedidos.push(newPedido);
+        });
+      });
+  }
 
   ngOnInit(): void {
+  }
+
+  toPedidoDetail(id: string): void {
+    this.router.navigate(['/admin/pedidos/' + id]);
   }
 
 }
