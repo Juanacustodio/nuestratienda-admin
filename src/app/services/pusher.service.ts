4cc2c7f82dfe55d5b8afd35363022f097e03f2e8
Juana Custodio <custodim.juana@gmail.com>
2021-02-20 17:04:03 -0500

    refactor pusher service

--- src/app/services/pusher.service.ts
+++ src/app/services/pusher.service.ts
@@ -9,20 +9,15 @@ export class PusherService {
   constructor() {
     this.pusher = new Pusher(environment.pusher.key, {
       cluster: environment.pusher.cluster,
-      authEndpoint: 'https://nta-admin.herokuapp.com/pusher/auth'
+      authEndpoint: `${environment.ntApiEndpoint}/pusher/auth`
     });
   }
 
   trigger(channel: string, eventName: string): void {
     const self = this;
-    const c = this.pusher.subscribe('private-' + channel);
+    const c = this.pusher.subscribe(`private-${channel}`);
     c.bind('pusher:subscription_succeeded', () => {
-      console.log('here');
-      self.pusher.send_event('client-' + eventName, {}, 'private-' + channel);
-    });
-
-    c.bind('pusher:subscription_error', (err: any) => {
-      console.log(err);
+      self.pusher.send_event(`client-${eventName}`, {}, `private-${channel}`);
     });
   }
 }
