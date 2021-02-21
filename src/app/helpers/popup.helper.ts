a94455400e0faa0d85afa969c9533ed2f649c7f9
Juana Custodio <custodim.juana@gmail.com>
2021-02-21 13:48:43 -0500

    popup helper with title and text

--- src/app/helpers/popup.helper.ts
+++ src/app/helpers/popup.helper.ts
@@ -1,21 +1,25 @@
-import Swal from 'sweetalert2';
+import Swal, {SweetAlertOptions} from 'sweetalert2';
+import * as _ from 'lodash';
 
 export class PopupHelper {
   timer = 1500;
-  showSuccessPopup(message: string): void {
-    Swal.fire({
-      icon: 'success',
-      title: message,
+  private showPopup(icon: 'success'|'error', title: string, text: string = ''): void {
+    const options: SweetAlertOptions = {
+      icon,
+      title,
       showConfirmButton: false,
       timer: this.timer
-    });
+    };
+    if (_.isEmpty(text)) {
+      options.text = text;
+    }
+
+    Swal.fire(options);
   }
-  showErrorPopup(message: string): void {
-    Swal.fire({
-      icon: 'error',
-      title: message,
-      showConfirmButton: false,
-      timer: this.timer
-    });
+  showSuccessPopup(title: string, text: string = ''): void {
+    this.showPopup('success', title, text);
+  }
+  showErrorPopup(title: string, text: string = ''): void {
+    this.showPopup('error', title, text);
   }
 }
